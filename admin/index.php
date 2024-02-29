bbeb013160519e4467fbac872edd247d22c42f7a
Kongfa Waroros <gongpha@hotmail.com>
2024-02-29 14:38:47 +0700

    add simple login page in admin folder

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<?php
+	?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+	<div class="col d-flex justify-content-center" style="margin-top: 64px">
+		<div class="card" style="min-width:1000px">
+			<div class="card-body">
+				<form>
+					<h2>หน้าผู้ดูแล</h2>
+					<div class="mb-3">
+						<label for="username" class="form-label">ชื่อผู้ใช้</label>
+						<input type="text" class="form-control" id="username">
+					</div>
+					<div class="mb-3">
+						<label for="password" class="form-label">รหัสผ่าน</label>
+						<input type="password" class="form-control" id="password">
+					</div>
+					<div class="mb-3">
+					<button type="submit" class="btn btn-primary">เข้าสู่ระบบ</button>
+					</div>
+				</form>
+			</div>
+		</card>
+	</div>
+</body>
+</html>
