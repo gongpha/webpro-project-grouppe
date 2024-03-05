aa4a94bac2f35b937a79af68aad2093350bfbd15
nkarGn <tabgamettv@gmail.com>
2024-03-05 12:16:35 +0700

    add profile pages

---
+++ studentProf.php
@@ -0,0 +1,131 @@
+
+
+<?php
+require 'template_init.php';
+require 'template_header.php';
+require 'template_container_begin.php';
+?>
+
+<style>
+
+    h4{
+        width: 300px;
+        font-size: 32px;
+    }
+
+    .card-img-overlay{
+        width:100px;
+        height: 100px;
+    }
+
+
+</style>
+
+<body>
+<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" height= "1000">
+           
+   
+<div class="container">
+	<div class="card text-bg-dark">
+		<img style="filter: brightness(25%);" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fobs.line-scdn.net%2F0hStzQr5r9DFtHKCNuO2BzDH1-DzR0RB9YIx5dWBtGUm9iHEgLe04UNWQuAjk5TEsFLk5LP2IhF2ptER8Fck8U%2Fw1200&f=1&nofb=1&ipt=2eaad6cce529a4c3dbb3fcc19c31f035c8a8c977ae8431e98b1a690710cbcd41&ipo=images;height=140" height="320" class="card-img" alt="...">
+		<div class="card-img-overlay">
+
+			<div class="d-flex align-items-center text-white text-decoration-none justify-content-between">
+				<img  src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.tnews.co.th%2Fnewscenter%2Fimages%2Fuserfiles%2Fimages%2F3-1(479).jpg&f=1&nofb=1&ipt=afa2128701b4e431b0e1ef3b4de7de5e80c6fa38d005486ad096ecc3709aec87&ipo=images" alt="avatar" width="256" height="256" class="rounded-circle me-4">
+				<div class="profDetail" width="400">
+					<h4>โจ๊ค ไอศกรีม</h4>
+					<small>สร้างเมื่อ 93 กันยาคม 2024</small>
+				</div>
+                
+
+				<br><br>
+            </div>
+            
+            
+        </div>
+        
+        
+    </div>
+    <div class="container">
+	<!--form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+		<input type="search" class="form-control form-control-dark text-bg-dark" placeholder="ค้นหาคอร์ส..." aria-label="Search">
+	</form-->
+	<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+		<div class="col-md-3 mb-2 mb-md-0">
+			<a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+				<svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+			</a>
+		</div>
+	</header>
+	
+	<h3 class="result">คอร์สของฉัน</h3>	<div class="col-md-3 text-end"></div>
+	<div class="row">
+		<div class="col-sm">
+								<div class="card mb-3">
+						<img src="https://media.discordapp.net/attachments/746159419091582997/1194887870960377957/FB_IMG_1704953799917.jpg?ex=65f2968c&amp;is=65e0218c&amp;hm=4f9620e8ae1722774bc4cba156079d4f8eaf310463610ebd5b3865502c98e926&amp;=&amp;format=webp&amp;width=473&amp;height=140" class="card-img-top object-fit-cover" height="400" alt="course cover">
+						<div class="card-body">
+							<div class="d-flex justify-content-between">
+								<h3 class="card-title">วิชาดาบ</h3>
+							</div>
+							
+							<p class="card-text">เล่นดาบกัน</p>
+							<p class="card-text">
+								<span class="badge text-bg-secondary">วิชาชีวิต</span>
+							</p>
+											<form action="action.php" method="post">
+					<input type="hidden" name="action" value="shopping_add_course">
+					<input type="hidden" name="course_id" value="1">
+					<input type="hidden" name="redirect_page" value="course_list.php">
+					
+					<a href="course_detail.php?id=1" class="btn btn-outline-secondary">ดูรายละเอียด</a>				</form>						</div>
+					</div>
+										<div class="card mb-3">
+						<img src="https://cdn.discordapp.com/attachments/974356275825627156/1212792127617245194/20231029_142726.jpg?ex=65f31faf&amp;is=65e0aaaf&amp;hm=8314ad749faa21191c7e6ca6f6f8cbd62640a9e5a87c90ff21b51190300385a4&amp;" class="card-img-top object-fit-cover" height="400" alt="course cover">
+						<div class="card-body">
+							<div class="d-flex justify-content-between">
+								<h3 class="card-title">สูตรลับ พ่อมึงตาย</h3>
+							</div>
+							
+							<p class="card-text">let him cook</p>
+							<p class="card-text">
+								<span class="badge text-bg-secondary">กูอยากตาย</span>
+							</p>
+											<form action="action.php" method="post">
+					<input type="hidden" name="action" value="shopping_add_course">
+					<input type="hidden" name="course_id" value="2">
+					<input type="hidden" name="redirect_page" value="course_list.php">
+					
+
+					<a href="course_detail.php?id=2" class="btn btn-outline-secondary">ดูรายละเอียด</a>				</form>						</div>
+					</div>
+					</div><div class="col-sm">					<div class="card mb-3">
+						<img src="https://cdn.discordapp.com/attachments/452857558596714509/1197232583533142047/image.png?ex=65f1e3bb&amp;is=65df6ebb&amp;hm=ba4b38b9f727ad8e772d36fd22d35fb8014fa18d9e7a2bf052ac3ff123a248b7&amp;" class="card-img-top object-fit-cover" height="400" alt="course cover">
+						<div class="card-body">
+							<div class="d-flex justify-content-between">
+								<h3 class="card-title">i learn for live</h3>
+							</div>
+							
+							<p class="card-text">do not even mention that</p>
+							<p class="card-text">
+								<span class="badge text-bg-secondary">เขียนโปรแกรม</span>
+							</p>
+											<form action="action.php" method="post">
+					<input type="hidden" name="action" value="shopping_add_course">
+					<input type="hidden" name="course_id" value="3">
+					<input type="hidden" name="redirect_page" value="course_list.php">
+					
+	
+					<a href="course_detail.php?id=3" class="btn btn-outline-secondary">ดูรายละเอียด</a>				</form>						</div>
+					</div>
+							</div>
+	</div>
+</div>
+</div>
+
+</ul>
+
+
+<?php
+require 'template_container_end.php';
+require 'template_footer.php';
+?>
